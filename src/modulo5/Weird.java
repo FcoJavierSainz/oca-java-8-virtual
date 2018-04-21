d703ec977a2844d7a01ce053cc2c6efd4864b4d7
Javier Sainz <jsainz@MacBook-Pro-de-Javier.local>
2018-04-21 09:07:16 -0500

    Modulo 5

---
+++ src/modulo5/Weird.java
@@ -0,0 +1,16 @@
+package modulo5;
+
+public class Weird {
+
+  public static void main(String[] args) {
+    int golpes = 1;
+    int par = 5;
+    int diferencia = golpes - par;
+    String resultado = "";
+    if (golpes == 1);
+    else
+      resultado = "Bogey!";
+    System.out.println(resultado);
+
+  }
+}
