f604b760efce7c635cc61643d7e5e1207b8cdbaf
Javier Sainz <jsainz@MacBook-Pro-de-Javier.local>
2018-04-21 11:19:19 -0500

    Middle of the class

--- src/modulo5/ForExercise.java
+++ src/modulo5/ForExercise.java
@@ -4,9 +4,20 @@ public class ForExercise {
 
   public static void main(String[] args) {
     // resolve this problem https://codefights.com/arcade/code-arcade/labyrinth-of-nested-loops/yBFfNv5fTqhcacZ7w
+
+    System.out.println(countHi("hi hi Hola hihihihihihi "));
   }
 
   public static int countHi(String input) {
-    return 0;
+    int count = 0;
+    for (int lastIndex = 0; lastIndex > -1; ) {
+      lastIndex = input.indexOf("hi", lastIndex);
+      if (lastIndex > -1) {
+        count++;
+        lastIndex += 2;
+      }
+    }
+    return count;
   }
+
 }
